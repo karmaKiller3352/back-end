7788fac7cb6dad65a84e9b87b0a23dbf262f6bd9
karmaKiller3352 <alievsul@gmail.com>
2020-09-07 00:41:26 +0300

    all-articles-by-cat-add

--- models/Category.js
+++ models/Category.js
@@ -1,29 +1,33 @@
 const mongoose = require('mongoose');
 
 const CategorySchema = mongoose.Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	content: {
-		type: String,
-		required: true,
-	},
-	date: {
-		type: Date,
-		default: Date.now,
-	},
-	url: {
-		type: String,
-		unique: true,
-		required: true,
-	},
-	metaTitle: {
-		type: String,
-	},
-	metaDesc: {
-		type: String,
-	},
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  url: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  metaTitle: {
+    type: String,
+  },
+  metaDesc: {
+    type: String,
+  },
+  articles: {
+    type: Array,
+    default: [],
+  },
 });
 
 module.exports = mongoose.model('Category', CategorySchema);
